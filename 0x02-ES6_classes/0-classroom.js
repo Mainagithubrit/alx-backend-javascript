fe29c97074b9a97938ffb190e78c5601b825d4c9
Mainagithubrit <njorogrfrancismaina@gmail.com>
2024-10-09 20:49:17 +0300

    Task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -3,11 +3,11 @@
  */
 
 export default class ClassRoom {
-	/**
-	 * creates a new class
-	 * with students and a class
-	 */
-	constructor(maxStudentsSize) {
-		this._maxStudentsSize = maxStudentsSize;
-	}
+  /**
+   * creates a new class
+   * with students and a class
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
