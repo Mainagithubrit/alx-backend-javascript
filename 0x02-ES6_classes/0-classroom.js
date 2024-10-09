6e75a5c1f9b34cd461617b03e26838972d808a2a
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-10-09 16:51:45 +0300

    Classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom
+ */
+
+export default class ClassRoom {
+	/**
+	 * creates a new class
+	 * with students and a class
+	 */
+	constructor(maxStudentsSize) {
+		this._maxStudentsSize = maxStudentsSize;
+	}
+}
