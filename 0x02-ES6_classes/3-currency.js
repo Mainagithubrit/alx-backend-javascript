80e76ab751b2fb5510b6e8a84d212d7ce6915ef4
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-10-10 14:33:00 +0300

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+/**
+ * A class for currencies
+ *
+ */
+
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+	set name(value) {
+		this._name = value;
+	}
+
+  /**
+ * Creates a string representation of currency
+ */
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
